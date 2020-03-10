7464eef6c4aba3a86402f1875372b1df0cee1c37
Zonghan-Barry-Gan <54106564+Zonghan-Barry-Gan@users.noreply.github.com>
2020-03-10 18:53:54 +0000

    200310-19-30 srvr end faster rsv bsd with ll slot
    To make the thread delay smaller with event base (qt signal-slot), the main slot (pong gaming) is spit in several parallel slot triggered by rsv event. The main slot currently only works for move the ball, which is very fast and even executed twice in 1 ms as long as the sig is fired fast enough. all the direction counting, bouncing and scoring are in ll slot

---
+++ 200310-19-30-Server end_pong-rs-event-based-pllslot/Pong.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-05-29T18:19:15
+#
+#-------------------------------------------------
+
+QT += core gui network
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Pong
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ 200310-19-30-Server end_pong-rs-event-based-pllslot/mainwindow.h
@@ -0,0 +1,67 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QObject>
+#include <QGraphicsScene>
+#include <QMainWindow>
+#include <QObject>
+#include <QUdpSocket>
+#include <QElapsedTimer>
+
+class QGraphicsItem;
+class QTimer;
+
+class Gameplay;
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    Ui::MainWindow *ui;
+    Gameplay *iLoop;
+    int iScore;
+
+    //graphic item in gameplay
+    QGraphicsScene * iScene;
+    QGraphicsRectItem *iP2, *iP1;
+    QGraphicsEllipseItem *iBall;
+    QTimer *iTimer;
+    QElapsedTimer timer_measure;
+
+    QPointF iBallMotion;
+    qreal iP2Motion;
+    qreal iP1Motion;
+    qreal CpuP1Motion();
+    //QUdpSocket *rsverSocket;
+public slots:
+    void refreshScore(int count);
+    void Position();
+    void Boundary();
+    void P2Moveleft();
+    void P2Moveright();
+
+
+
+signals:
+    void goal(int player);
+    void P2isleft();
+    void P2isright();
+
+
+//for udp receive
+public slots:
+    void receive();
+
+private:
+    QUdpSocket *rsverSocket;
+};
+
+#endif // MAINWINDOW_H
